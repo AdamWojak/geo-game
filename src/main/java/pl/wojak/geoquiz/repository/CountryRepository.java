a053e4360e702d0b6639917498c076b216bd1f8a
AdamWojak <adam.wojak@nekken.pl>
2019-01-14 10:29:10 +0100

    sql sample for 3 countries whole world in HQL

--- src/main/java/pl/wojak/geoquiz/repository/CountryRepository.java
+++ src/main/java/pl/wojak/geoquiz/repository/CountryRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.wojak.geoquiz.entity.CountryEntity;
-import pl.wojak.geoquiz.enums.AreaEnum;
 import pl.wojak.geoquiz.enums.DifficultyLevelEnum;
 
 import java.util.List;
@@ -21,6 +20,7 @@ public interface CountryRepository extends CrudRepository<CountryEntity, Long> {
     @Query(value = "select * from geo_schema.country where continent=?2 and level<=?3 and id not in(select country_id from geo_schema.guessed g where g.game_id=?1) order by random() limit 3", nativeQuery = true)
     List<CountryEntity> findRandom3CountriesForOneContinent(Long id, String continent, Integer level);
 
+    //    @Query(value = "select c from CountryEntity c where level<=?2 and id not in(select country from GuessedEntity g where g.game=?1) order by random()")
     @Query(value = "select * from geo_schema.country where level<=?2 and id not in(select country_id from geo_schema.guessed g where g.game_id=?1) order by random() limit 3", nativeQuery = true)
     List<CountryEntity> findRandom3CountriesForWholeWorld (Long id, Integer level);
 
